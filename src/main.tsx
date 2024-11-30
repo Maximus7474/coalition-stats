4bd88ac3101500b32e95cdcae1ca91a1956a89d8
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-30 17:30:36 +0100

    rf(main): added Mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const RootComponent = () => {
+  return (
+    <StrictMode>
+        <MantineProvider defaultColorScheme="auto">
+          <App />
+        </MantineProvider>
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById('root')!).render(<RootComponent />);
