511d74d346b66db33ac3950d00b2c89c816baa60
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-01 23:33:25 +0100

    rm: testing code for shit rfs commits -> was it worth it, prob not

--- api/auth/login.js
+++ api/auth/login.js
@@ -10,11 +10,6 @@ export default function handler(req, res) {
     if (HOST_URL.includes('localhost') && !HOST_URL.startsWith('http')) redirectUri = `http://${HOST_URL}`;
     else if (HOST_URL.includes('vercel') && !HOST_URL.startsWith('http')) redirectUri = `https://${HOST_URL}`;
 
-    console.log('Using', redirectUri, 'as redirect uri');
-    console.log('Raw Shit:', HOST_URL, redirectUri);
-    console.log('1.', HOST_URL.includes('localhost') && !HOST_URL.startsWith('http'));
-    console.log('2.', HOST_URL.includes('vercel') && !HOST_URL.startsWith('http'));
-
     const params = new URLSearchParams({
         client_id: DISCORD_CLIENT_ID,
         redirect_uri: `${redirectUri}/api/auth/callback`,
