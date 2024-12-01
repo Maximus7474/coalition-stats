2caa58762e96fa6ce5162e9836766395317d7ce0
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-01 23:18:57 +0100

    rf shit 4x

--- api/auth/login.js
+++ api/auth/login.js
@@ -7,8 +7,8 @@ export default function handler(req, res) {
 
     let redirectUri;
 
-    if (HOST_URL.includes('localhost') && !HOST_URL.startsWith('http')) redirectUri = `http://${HOST_URL}/api/auth/callback`;
-    else if (HOST_URL.includes('vercel') && !HOST_URL.startsWith('http')) redirectUri = `https://${HOST_URL}/api/auth/callback`;
+    if (HOST_URL.includes('localhost') && !HOST_URL.startsWith('http')) redirectUri = `http://${HOST_URL}`;
+    else if (HOST_URL.includes('vercel') && !HOST_URL.startsWith('http')) redirectUri = `https://${HOST_URL}`;
 
     console.log('Using', redirectUri, 'as redirect uri');
     console.log('Raw Shit:', HOST_URL, redirectUri);
@@ -17,7 +17,7 @@ export default function handler(req, res) {
 
     const params = new URLSearchParams({
         client_id: DISCORD_CLIENT_ID,
-        redirect_uri: redirectUri,
+        redirect_uri: `${redirectUri}/api/auth/callback`,
         response_type: 'code',
         scope: 'identify email',
     });
