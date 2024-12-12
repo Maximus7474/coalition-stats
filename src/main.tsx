a606ce017ca767c18a1d60d35ad6045dd093a2d5
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-12 19:37:31 +0100

    chore: added mantine charts

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import App from './App.tsx';
+
+import '@mantine/core/styles.css';
+import '@mantine/charts/styles.css';
 
 const RootComponent = () => {
   return (
